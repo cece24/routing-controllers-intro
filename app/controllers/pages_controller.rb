c6b19702635e9129a37b36a07842f796fe89d02d
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-11 15:16:48 -0400

    Add route and action for /kitten, will render a randomized kitten photo of size specified in the URL

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,9 @@ class PagesController < ApplicationController
   def contest
     @header = "Contest Details"
   end
+
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
 end
