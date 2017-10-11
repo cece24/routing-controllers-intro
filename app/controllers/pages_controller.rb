cbefb91afb4d5c355faadc7a2fbd8abfd80ff0b4
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-11 15:10:21 -0400

    Define @header variable for Pages methods to render h1 in layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,13 @@
 class PagesController < ApplicationController
   def welcome
-
+    @header = "Welcome!"
   end
 
   def about
-
+    @header = "About this site"
   end
 
   def contest
-
+    @header = "Contest Details"
   end
 end
